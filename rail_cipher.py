0a092e308838071ba6f77e9787942d92107b4fdb
Leo <fleonardolewis@gmail.com>
2020-07-23 21:27:48 -0500

    Added tests and comments

--- rail_cipher.py
+++ rail_cipher.py
@@ -1,20 +1,26 @@
 #Rail cipher from the civil war
 #Impractical python projects chapter 4
 
-#
+#main method to receive plain text and pass to encryption method
 def main():
     plain_text = str(input("Enter plaintext:"))
     encrypt(plain_text)
 
+#Encryption method
 def encrypt(plaintext):
+    #convert to upper case, split and then rejoin with each char as one element in one list
     plain_list = list("".join(plaintext.upper().split()))
+    #divide into two lists, one with odd indexes from original list and one with even indexes
     list_one = plain_list[::2]
     list_two = plain_list[1::2]
-    plain_list[::2] = list_one
-    plain_list[1::2] = list_two
+    #place elements back into original list
+    #every other char from original list, folllowed by every other char from second half of orginal list
     cipher_text = list_one + list_two
+    #convert to string
     rails = ''.join(cipher_text)
-    print(' '.join(rails[i:i+5] for i in range(0, len(rails), 5)))
+    #place space every 5 characters 
+    cipher_text = ' '.join(rails[i:i+5] for i in range(0, len(rails), 5))
+    return cipher_text
     
  
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
-import transposition_decrypt
+import transposition_decrypt, rail_cipher
 
-def test():
+def test_transposition():
     ciphertext = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"
     plaintext = "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 "
     cols = 4
     rows = 5
     assert transposition_decrypt.decrypt(ciphertext,cols, rows) == plaintext
 
-test()
+def test_rail():
+    plaintext = "Buy more Maine potatoes"
+    ciphertext = "BYOEA NPTTE UMRMI EOAOS"
+    assert rail_cipher.encrypt(plaintext) == ciphertext
+
+
+def main():
+    test_transposition()
+    test_rail()
+
+
+if __name__ == "__main__":
+    main()
